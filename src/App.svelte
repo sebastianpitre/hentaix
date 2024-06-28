2a739db8e003c2ec2309ac307903e800ca2598d7
sebastianpitre <sebastianandrespitrediaz@gmail.com>
2024-06-28 17:52:43 -0500

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Card from './components/card.svelte';
+  import Header from './components/header.svelte';
+
+</script>
+
+
+
+<main>
+  <Header/>
+  <div class="container">
+    <Card/>
+  </div>
+  
+</main>
---
+++ src/components/header.svelte
@@ -0,0 +1,33 @@
+<header class="bg-gradient-danger">
+    <div class="page-header min-vh-45" style="background-image: url(&quot;./bg.jpeg&quot;); background-position-y: 50%; background-size: cover; background-position-x: 70%;">
+      <span class="mask bg-gradient-dark opacity-8"></span>
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-lg-8 text-center mx-auto my-auto">
+
+            
+              <h1 class=" text-white mb-5 text-hentai mx-auto" style="font-size: 100px;">TuHentai</h1>
+            
+
+
+          </div>
+        </div>
+      </div>
+      <div class="position-absolute w-100 z-index-1 bottom-0">
+        
+        <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 40" preserveAspectRatio="none" shape-rendering="auto">
+          <defs>
+            <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"></path>
+          </defs>
+          <g class="moving-waves">
+            <use xlink:href="#gentle-wave" x="48" y="-1" fill="rgba(255,255,255,0.40"></use>
+            <use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.35)"></use>
+            <use xlink:href="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.25)"></use>
+            <use xlink:href="#gentle-wave" x="48" y="8" fill="rgba(255,255,255,0.20)"></use>
+            <use xlink:href="#gentle-wave" x="48" y="13" fill="rgba(255,255,255,0.15)"></use>
+            <use xlink:href="#gentle-wave" x="48" y="16" fill="#141414"></use>
+          </g>
+        </svg>
+      </div>
+    </div>
+  </header>
